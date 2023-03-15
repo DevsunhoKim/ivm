e326e5aa2ee923bb31c1cb536a59c3b10cae5b6a
DevsunhoKim <ksssk96@gmail.com>
2023-03-15 23:30:11 +0900

    23.3.15

--- script.js
+++ script.js
@@ -213,3 +213,50 @@ loginForm.addEventListener('click', (event) => {
 // id 별 데이터 테이블 서버저장
 // 보안상 안전한 로그인 시스템 구현
 // 데이터 처리방식 구현
+
+/*<?php
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+require_once "config.php";
+$username = $password = "";
+$username_err = $password_err = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    if(empty($username_err) && empty($password_err)){
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            $param_username = $username;
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            session_start();
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            header("location: welcome.php");
+                        } else{
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "
+*/
